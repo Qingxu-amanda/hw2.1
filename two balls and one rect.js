47fc1f1dfa19c84b3df8ffbba47561bb46ef4b65
Qingxu-amanda <55409415+Qingxu-amanda@users.noreply.github.com>
2019-09-23 14:50:58 -0700

    Create two balls and one rect.js

---
+++ two balls and one rect.js
@@ -0,0 +1,54 @@
+function setup() {
+  createCanvas(400, 400);
+  frameRate(10);
+  fill(0,255,0);
+}
+
+var NMSL = 30;
+var ballSpeed = 3;
+
+var ballX2 = 370
+var ballSpeed2 = 5;
+
+var nmjwbs = 0;
+var nmjwbsl = 4;
+
+function draw() {
+  background(220);
+  
+  fill(255, 0, 0)
+  ellipse(mouseX, NMSL, 30);
+  fill(0, 255, 0)
+  ellipse(mouseY, ballX2, 30);
+  fill(0, 0, 255)
+  rect(100, nmjwbs, 30, 100)
+  
+  NMSL = NMSL+ballSpeed;
+  ballX2 = ballX2 + ballSpeed2;
+  nmjwbs = nmjwbs + nmjwbsl
+  
+  if(NMSL >= 400) {
+   ballSpeed = -3;
+  }
+  
+  if(ballX2 >=400) {
+    ballSpeed2 = -5;
+  }
+  
+  if(NMSL <= 0) {
+    ballSpeed = 3;
+  }
+  
+  if(ballX2 <=0) {
+    ballSpeed2 =5
+  }
+  
+  if(nmjwbs >=400){
+    nmjwbsl = -4
+  }
+  
+  if(nmjwbs <=0){
+    nmjwbsl =4
+  }
+
+}
